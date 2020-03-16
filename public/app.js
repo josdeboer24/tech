0d661f5821f0773af9481d54c4c08c77615e4638
josdeboer24 <jhc.dbr@gmail.com>
2020-03-16 19:44:09 +0100

    mongo werkt nog niet

---
+++ public/app.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+require('dotenv').config()
+var bodyParser=require("body-parser");
+const mongoose  = require('mongoose')
+
+
+console.log("hoihoi");x
